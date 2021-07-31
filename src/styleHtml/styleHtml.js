0dfdb38395f8723be85152ddf5a9794b696e2af7
mehan <mehan.alavi.88.majd@gmail.com>
2021-07-31 11:58:47 +0430

    deleted styleHtml

--- src/styleHtml/styleHtml.js
+++
@@ -1,8 +0,0 @@
-export const styleHtml = (obj, container) => {
-  let css = style(obj);
-  let fullString = "";
-  css.forEach((el) => {
-    fullString += el;
-  });
-  container.setAttribute("style", fullString);
-};
--- tests/styleHtml.test.js
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from "./index.js";
-import { mount, node, styleHtml } from "../dist/panix.js";
-let a = mount(node("h1", { id: "hello2" }, "hello"), document.body);
-styleHtml({ color: "red" }, a);
-describe("styleHtml tests", () => {
-  it("correct attr", () => {
-    expect(document.getElementById("hello2").style.color).toBe("red");
-  });
-});
