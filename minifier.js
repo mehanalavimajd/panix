56ed256f5f0f3e66f2a354cb2a41193393707669
mehan <mehan.alavi.88.majd@gmail.com>
2021-09-06 21:44:17 +0430

    added minifier

---
+++ minifier.js
@@ -0,0 +1,11 @@
+let glob = require("glob");
+let fs = require("fs");
+const uglify = require("uglify-js");
+
+
+function minify(filein , fileout) {
+  fs.writeFileSync(fileout , uglify.minify(fs.readFileSync(filein).toString()).code)
+}
+minify("./dist/panix.cjs.js" , "./dist/panix.cjs.min.js")
+minify("./dist/panix.es.js" , "./dist/panix.es.min.js")
+minify("./dist/panix.umd.js" , "./dist/panix.umd.min.js")
