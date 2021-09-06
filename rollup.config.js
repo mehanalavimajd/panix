83f1edf72ed29e19204c76105cf1b252195c1d06
mehan <mehan.alavi.88.majd@gmail.com>
2021-09-06 21:48:52 +0430

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,34 @@
-
 export default {
-    // use glob in the input
+  // use glob in the input
   input: "./src/index.js",
-   output: [
-     {
-      format: 'es',
-      file:"dist/panix.es.js"
-     },
-     {
-      format: 'es',
-      file:"dist/panix.es.min.js"
-     },
-     {
-      format: 'umd',
-      file:"dist/panix.umd.js",
-      name:"panix"
-     },
-     {
-      format: 'umd',
-      file:"dist/panix.umd.min.js",
-      name:"panix"
-     },
-     {
-      format: 'cjs',
-      file:"dist/panix.cjs.js",
-      name:"panix"
-     },
-     {
-      format: 'cjs',
-      file:"dist/panix.cjs.min.js",
-      name:"panix"
-     },
-
-    ]
-}
+  output: [
+    {
+      format: "es",
+      file: "dist/panix.es.js",
+    },
+    {
+      format: "es",
+      file: "dist/panix.es.min.js",
+    },
+    {
+      format: "umd",
+      file: "dist/panix.umd.js",
+      name: "panix",
+    },
+    {
+      format: "umd",
+      file: "dist/panix.umd.min.js",
+      name: "panix",
+    },
+    {
+      format: "cjs",
+      file: "dist/panix.cjs.js",
+      name: "panix",
+    },
+    {
+      format: "cjs",
+      file: "dist/panix.cjs.min.js",
+      name: "panix",
+    },
+  ],
+};
