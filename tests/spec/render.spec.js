675d3861e4148e81c7da80cb465806a078a013b2
mehan <mehan.alavi.88.majd@gmail.com>
2022-08-24 17:40:29 +0430

    Added render tests

--- tests/spec/render.spec.js
+++ tests/spec/render.spec.js
@@ -1,8 +1,8 @@
-import {render,node} from "../../src/index.js"
+import { render, node } from "../../src/index.js";
 
 describe("Render Tests", () => {
-    it("Render is ok",()=>{
-        render(node("h1",{id:'hello'},"hello"),document.body)
-        expect(document.getElementById("hello").textContent).toEqual("hello")
-    })
+  it("Render is ok", () => {
+    render(node("h1", { id: "hello" }, "hello"), document.body);
+    expect(document.getElementById("hello").textContent).toEqual("hello");
+  });
 });
